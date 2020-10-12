c24495cb9b773526e8bc76b6cb8469df412c7c36
robertadelima <robertarl@gmail.com>
2020-10-12 17:06:04 -0300

    Create component title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+function Title () {
+  return (
+    <h1>Hello World from Title</h1>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title />
     </div>
   );
 }
